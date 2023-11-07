c851125f4b264a873cf2cb09144a7b6f4871a7b5
ndw108 <30444693+ndw108@users.noreply.github.com>
2023-11-07 11:29:28 +0000

    Add files via upload

---
+++ build/app/CMakeFiles/TG_MHD.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TG_MHD.dir/TG_MHD/TG_MHD.cpp.o"
+  "CMakeFiles/TG_MHD.dir/TG_MHD/TG_MHD.cpp.o.d"
+  "TG_MHD"
+  "TG_MHD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TG_MHD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/app/CMakeFiles/TG_MHD.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TG_MHD.
+# This may be replaced when dependencies are built.
